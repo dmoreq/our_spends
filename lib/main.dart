6b1122c19675021322776b91167983164a6fead0
Quy Doan <quy.doanm@gmail.com>
2025-08-11 17:10:48 +0700

    build(deps): remove authentication-related dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'screens/auth_wrapper.dart';
-import 'providers/auth_provider.dart';
+import 'screens/home_screen.dart';
 import 'providers/expense_provider.dart';
 import 'providers/language_provider.dart';
 import 'theme/app_theme.dart';
@@ -11,25 +9,6 @@ import 'l10n/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  try {
-    // Initialize Firebase with demo configuration for web
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: 'demo-api-key',
-        appId: '1:123456789:web:demo-app-id',
-        messagingSenderId: '123456789',
-        projectId: 'demo-project-id',
-        authDomain: 'demo-project-id.firebaseapp.com',
-        storageBucket: 'demo-project-id.appspot.com',
-        measurementId: 'G-DEMO123456',
-      ),
-    );
-  } catch (e) {
-    // Continue without Firebase for demo purposes
-    // Firebase initialization error ignored in demo mode
-  }
-  
   runApp(const OurSpendsApp());
 }
 
@@ -41,7 +20,6 @@ class OurSpendsApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => LanguageProvider()),
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => ExpenseProvider()),
       ],
       child: Consumer<LanguageProvider>(
@@ -62,7 +40,7 @@ class OurSpendsApp extends StatelessWidget {
               Locale('en', ''),
               Locale('vi', ''),
             ],
-            home: const AuthWrapper(),
+            home: const HomeScreen(),
             debugShowCheckedModeBanner: false,
           );
         },
